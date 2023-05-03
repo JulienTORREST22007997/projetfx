2245c63f40ba3066a55d2d06ff70154ac52ccdca
TORRES Julien <t22007997@A-F002094.salsa.univ-amu.fr>
2023-05-03 08:36:01 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module fr.julien.projetfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens fr.julien.projetfx to javafx.fxml;
+    exports fr.julien.projetfx;
+}
